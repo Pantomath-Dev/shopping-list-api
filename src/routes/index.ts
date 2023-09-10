73a0c96e3cfedaf772cf8a24bfa6b9ca49a1218e
Seth Jones <sethjones348@gmail.com>
2023-09-09 23:25:50 -0400

    Allow for CORS from localhost:3001

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as api from "./api"
 import ErrorResponse from "../types/ErrorResponse";
+import cors from "cors";
 
 const apiKeyValidator = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const apiKey = req.headers['api_key'];
@@ -23,6 +24,12 @@ export const register = (app: express.Application) => {
     app.get('/', (_req, _res) => {
         _res.send("You just hit the root");
     });
+
+    const corsOptions = {
+        origin: 'http://localhost:3001',
+    };
+
+    app.use(cors(corsOptions));
 
     app.use('/*', apiKeyValidator);
 
